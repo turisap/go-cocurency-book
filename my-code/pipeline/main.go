0a5845af6fa05728027e60d430335d8e49abba4d
Kirill Shakirov <kirillshakirov57@gmail.com>
2024-01-03 15:28:29 +0700

    task: add csv for pipelines

--- my-code/pipeline/main.go
+++ my-code/pipeline/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+func main() {
+	data := getCSV()
+	//synchronousPipeline(data)
+	//asyncPipeline(data)
+	asyncPipelineFan(data)
+}
+
+func getCSV() (reader *csv.Reader) {
+	f, err := os.Open("./data.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader = csv.NewReader(f)
+
+	return
+}
